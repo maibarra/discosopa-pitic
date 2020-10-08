6eeba42fc2c514947bac9ea80d75f1e08271bb9f
Marco Ibarra <yo@marcoibarra.com>
2020-10-07 17:57:35 -0700

    Index Page Translation

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -90,18 +90,18 @@ export const IndexPageTemplate = ({
                 <div className="columns">
                   <div className="column is-12 has-text-centered">
                     <Link className="btn" to="/products">
-                      See all products
+                      Nuestra Misión
                     </Link>
                   </div>
                 </div>
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
+                    Noticias más Recientes
                   </h3>
                   <BlogRoll />
                   <div className="column is-12 has-text-centered">
                     <Link className="btn" to="/blog">
-                      Read more
+                      Más Noticias
                     </Link>
                   </div>
                 </div>
