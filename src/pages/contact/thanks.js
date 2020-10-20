1201acc2008fa75df726ebb8480660fd4d3d55b3
Marco Ibarra <yo@marcoibarra.com>
2020-10-19 20:52:34 -0700

    Gracias: Proofread + Styling

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -7,9 +7,11 @@ export default () => (
       <div className="container">
         <div className="content">
           <h1>¡Gracias por tu aportación!</h1>
-          <h4>En este espacio estamos comprometidos en concientizarnos a nosotros mismos y a la comunidad por medio de quehaceres diarios.Con tu apoyo fondeamos futuras y próximas actividades sin fines de lucro del colectivo.
-</h4>
+          <h4>En este espacio estamos comprometidos con nuestra concientización y la de la comunidad por medio de acciones diarias.<br>
+          Con tu apoyo e ideas fondeamos actividades próximas y futuras del colectivo.<br>
+          Todas sin fines de lucro.</h4>
           <p>Mucho Amor y Buena Vibra </p>
+          <h5>Disco Sopa Pitic</h5>
         </div>
       </div>
     </section>
