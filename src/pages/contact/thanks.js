97367b6f41ecfb7950b089cd5f1584dee206216b
Marco Ibarra <yo@marcoibarra.com>
2020-10-19 21:00:30 -0700

    Gracias: Final Draft

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -7,9 +7,9 @@ export default () => (
       <div className="container">
         <div className="content">
           <h1>¡Gracias por tu aportación!</h1>
-          <h4>En este espacio estamos comprometidos con nuestra concientización y la de la comunidad por medio de acciones diarias.<br>
-          Con tu apoyo e ideas fondeamos actividades próximas y futuras del colectivo.<br/>
-          Todas sin fines de lucro.</h4>
+          <h4>En este espacio estamos comprometidos con nuestra concientización y la de la comunidad por medio de acciones diarias</h4>
+          <h4>Con tu apoyo e ideas fondeamos actividades próximas y futuras del colectivo.</h4>
+          <h4>Todas sin fines de lucro</h4>
           <p>Mucho Amor y Buena Vibra </p>
           <h5>Disco Sopa Pitic</h5>
         </div>
