7937d18bb12deb2c7305ecd015ff4cff364c8001
Marco Ibarra <yo@marcoibarra.com>
2020-10-19 20:56:18 -0700

    Gracias: JSX closing tag

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -8,7 +8,7 @@ export default () => (
         <div className="content">
           <h1>¡Gracias por tu aportación!</h1>
           <h4>En este espacio estamos comprometidos con nuestra concientización y la de la comunidad por medio de acciones diarias.<br>
-          Con tu apoyo e ideas fondeamos actividades próximas y futuras del colectivo.<br>
+          Con tu apoyo e ideas fondeamos actividades próximas y futuras del colectivo.<br/>
           Todas sin fines de lucro.</h4>
           <p>Mucho Amor y Buena Vibra </p>
           <h5>Disco Sopa Pitic</h5>
