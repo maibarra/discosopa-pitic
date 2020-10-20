16dcbe6260f5fe79c8ab38990e2b3433ccd6f2c2
Marco Ibarra <yo@marcoibarra.com>
2020-10-20 13:27:24 -0700

    Paypal donate

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -72,6 +72,11 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/contact">
                 Contacto
               </Link>
+              <form action="https://www.paypal.com/donate" method="post" target="_top">
+                <input type="hidden" name="hosted_button_id" value="SS88G8676FSBS" />
+                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donar con PayPal!" />
+                <img alt="" border="0" src="https://www.paypal.com/en_MX/i/scr/pixel.gif" width="1" height="1" />
+              </form>
             </div>
             <div className="navbar-end has-text-centered">
               <a
